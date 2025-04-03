baa74c8b4894bb42191a2809fdc5e0a0cd1bce50
Romyleads <romyleads@gmail.com>
2025-04-03 09:56:13 +0200

    add update homework 47

--- src/homeworks/hw_47/Task2/filterStringApp.java
+++ src/homeworks/hw_47/Task2/filterStringApp.java
@@ -2,8 +2,6 @@ package homeworks.hw_47.Task2;
 
 /*** Author: Roman Romashko Date: 03.04.2025 ***/
 
-import org.apiguardian.api.API;
-
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -20,18 +18,38 @@ public class filterStringApp {
         List<String> list = List.of("Avocado", "Banana", "Apple", "Cherry");
 
         // смотрим
-        System.out.println("list: "+list);
+        System.out.println("list: " + list);
 
         // фильтруем
-        List<String> result = filterAndSortStrings(list,"A");
+        List<String> result = filterAndSortStrings(list, "A");
 
         // смотрим
-        System.out.println("result: "+result);
+        System.out.println("result: " + result);
+
+        // теория
+
+        String test = "Hello";
+        System.out.println(test.substring(0, 1));
+        // проверяет, заканчивается ли строка на другую строку
+        System.out.println(test.endsWith("lle"));
+        System.out.println(test.endsWith("ll"));
+        // проверяет, начинаться ли строка на другую строку
+        System.out.println(test.startsWith("H"));
+
+
+        System.out.println(test.startsWith("ell"));
+
+        // проверяет, содержит ли строка другую строку
+        System.out.println(test.contains("ells"));
 
     }
 
     private static List<String> filterAndSortStrings(List<String> strings, String startsWith) {
         return strings.stream()
+                //.filter(a -> a.charAt(0) == 'A') // фильтр по символу 0
+                //.filter(a->a.substring(0,1).equalsIgnoreCase("A")) // по сабстроке
+                //.filter(a->a.toUpperCase().startsWith("A")) - по первой букве А с любым регистром
+
                 .filter(a -> a.startsWith(startsWith)) // фильтр на первую А
                 .sorted()// по алфавиту
                 .collect(Collectors.toList() // сборка
